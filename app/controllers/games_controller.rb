4b20fa3c48f3a07f3ca9b5cbaddc83a3bbb2b402
DavidRMorphew <morphewd@umich.edu>
2021-04-17 11:14:37 -0400

    GameSerializer user_id added, GamesController index action activerecord query modified

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,9 +1,8 @@
 class GamesController < ApplicationController
     def index
-        # orders games according to user
-        games = Game.all.order(user_id: :desc)
+        games = Game.all.joins(:user).order(:user_id)
+        # order(user_id: :desc)
         render json: GameSerializer.new(games)
-        # use the current user to put that user at the top
         # Game.order(user_win: :desc) [winning on top]
         # Game.order(user_switch: :desc) [user_switch true on top]
     end
--- app/serializers/game_serializer.rb
+++ app/serializers/game_serializer.rb
@@ -1,4 +1,4 @@
 class GameSerializer
   include FastJsonapi::ObjectSerializer
-  attributes :door1, :door2, :door3, :original_pick, :host_reveal, :user_switch, :user_win, :user_name
+  attributes :door1, :door2, :door3, :original_pick, :host_reveal, :user_switch, :user_win, :user_name, :user_id
 end

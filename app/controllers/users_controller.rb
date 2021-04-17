e70087710dbc81120205ea9417b088312d1be2d6
DavidRMorphew <morphewd@umich.edu>
2021-04-17 11:45:45 -0400

    index and show actions added to UsersController

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,11 @@
 class UsersController < ApplicationController
+    def index
+        users = User.all
+        render json: UserSerializer.new(users, include: [:games])
+    end
+    
+    def show
+        user = User.find(params[:id])
+        render json: UserSerializer.new(user)
+    end
 end

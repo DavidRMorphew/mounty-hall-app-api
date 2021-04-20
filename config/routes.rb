0cf81ba57386182fd39b816e609c7769e97e7c23
DavidRMorphew <morphewd@umich.edu>
2021-04-19 22:49:03 -0400

    nested route for user's games added

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   resources :games
-  resources :users
+  resources :users do
+    resources :games, only: [:index]
+  end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end

280573374e1a729088db53ebf73428d08a7d2d8d
DavidRMorphew <morphewd@umich.edu>
2021-04-23 17:31:20 -0400

    game_params added to GamesController

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,9 +1,18 @@
 class GamesController < ApplicationController
+    def create
+        binding.pry
+    end
+    
     def index
         games = Game.all.joins(:user).order(:user_id)
         # order(user_id: :desc)
         render json: GameSerializer.new(games)
         # Game.order(user_win: :desc) [winning on top]
         # Game.order(user_switch: :desc) [user_switch true on top]
+    end
+
+    private
+    def game_params
+        params.require(:game).permit(:door1, :door2, :door3, :original_pick, :host_reveal, :user_switch, :user_win, :user_id)
     end
 end

4a07aa263f6eb398f9c632f21fa4d8d4b3a3c00a
DavidRMorphew <morphewd@umich.edu>
2021-04-26 15:02:00 -0400

    User winning_game_percentage instance method added

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,9 @@
 class User < ApplicationRecord
     has_many :games
+
+    def winning_game_percentage
+        user_games_won = self.games.where(user_win: true).count
+        user_games_total = self.games.count
+        win_percentage = (user_games_won.to_f / user_games_total.to_f) * 100
+    end
 end

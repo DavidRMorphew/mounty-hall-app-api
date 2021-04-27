3b1f65b10ee2c6d798622d44ea744e031b8b53e8
DavidRMorphew <morphewd@umich.edu>
2021-04-27 09:48:27 -0400

    user switch percentage method added

--- app/models/user.rb
+++ app/models/user.rb
@@ -11,6 +11,16 @@ class User < ApplicationRecord
         end
     end
 
+    def switch_percentage
+        user_games_switch = self.games.where(user_switch: true).count
+        user_games_total = self.games.count
+        if (user_games_total != 0)
+            switch_percentage = (user_games_switch.to_f / user_games_total.to_f) * 100
+        else
+            switch_percentage = 0.0
+        end
+    end
+
     def self.ordered_by_game_winning_percentage
         self.all.sort_by do |user| 
             user.winning_game_percentage

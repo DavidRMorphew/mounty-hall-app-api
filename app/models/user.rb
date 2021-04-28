de06fa0c0f79bc440b1d8c270dd700b2da97bf74
DavidRMorphew <morphewd@umich.edu>
2021-04-28 10:44:41 -0400

    User switch_and_win_percentage and stay_and_win_percentage methods created and added to UserSerializer

--- app/models/user.rb
+++ app/models/user.rb
@@ -12,12 +12,32 @@ class User < ApplicationRecord
     end
 
     def switch_percentage
-        user_games_switch = self.games.where(user_switch: true).count
+        user_games_switch_count = self.games.where(user_switch: true).count
         user_games_total = self.games.count
         if (user_games_total != 0)
-            switch_percentage = (user_games_switch.to_f / user_games_total.to_f) * 100
+            switch_percentage = (user_games_switch_count.to_f / user_games_total.to_f) * 100
         else
             switch_percentage = 0.0
+        end
+    end
+
+    def switch_and_win_percentage
+        user_games_switch = self.games.where(user_switch: true)
+        user_games_switch_and_win = user_games_switch.where(user_win: true)
+        if (user_games_switch.count != 0)
+            switch_and_win_percentage = (user_games_switch_and_win.count.to_f / user_games_switch.count.to_f) * 100
+        else
+            switch_and_win_percentage = 0.0
+        end
+    end
+
+    def stay_and_win_percentage
+        user_games_stay = self.games.where(user_switch: false)
+        user_games_stay_and_win = user_games_stay.where(user_win: true)
+        if (user_games_stay.count != 0)
+            stay_and_win_percentage = (user_games_stay_and_win.count.to_f / user_games_stay.count.to_f) * 100
+        else
+            stay_and_win_percentage = 0.0
         end
     end
 
--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -1,5 +1,5 @@
 class UserSerializer
   include FastJsonapi::ObjectSerializer
   has_many :games
-  attributes :name, :winning_game_percentage, :switch_percentage
+  attributes :name, :winning_game_percentage, :switch_percentage, :switch_and_win_percentage, :stay_and_win_percentage
 end

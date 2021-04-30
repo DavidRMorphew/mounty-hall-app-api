29be6fe5f03885eecc46bc64da7e59deba862939
DavidRMorphew <morphewd@umich.edu>
2021-04-29 22:26:36 -0400

    UsersController show never used and removed

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,12 +3,6 @@ class UsersController < ApplicationController
         users = User.ordered_by_game_winning_percentage
         render json: UserSerializer.new(users, include: [:games])
     end
-    
-    # def show
-    #     binding.pry
-    #     user = User.find(params[:id])
-    #     render json: UserSerializer.new(user)
-    # end
 
     def create
         user = User.find_or_create_by(user_params)

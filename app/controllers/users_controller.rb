b7d1a752f70a91a06f724e98421d6244d88feb9b
DavidRMorphew <morphewd@umich.edu>
2021-04-29 22:32:42 -0400

    routes cleaned up

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,14 +1,14 @@
 class UsersController < ApplicationController
-    def index
-        users = User.ordered_by_game_winning_percentage
-        render json: UserSerializer.new(users, include: [:games])
-    end
-
     def create
         user = User.find_or_create_by(user_params)
         render json: UserSerializer.new(user)
     end
-
+    
+    def index
+        users = User.ordered_by_game_winning_percentage
+        render json: UserSerializer.new(users, include: [:games])
+    end
+    
     private
     def user_params
         params.require(:user).permit(:name)
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,6 @@
 Rails.application.routes.draw do
-  resources :games
-  resources :users do
+  resources :games, only: [:create, :index]
+  resources :users, only: [:create, :index] do
     resources :games, only: [:index]
   end
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
